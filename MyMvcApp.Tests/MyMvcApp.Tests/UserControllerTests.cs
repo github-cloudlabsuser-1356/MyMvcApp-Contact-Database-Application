9ebd18350fe69061db83b8601c1325c85117a263
Pratibha <github_cloudlabsuser_1356@cloudlabsaiuser.com>
2025-05-23 13:21:45 +0000

    Changes

---
+++ MyMvcApp.Tests/MyMvcApp.Tests/UserControllerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MyMvcApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Example test: Assert that true is true
+        Assert.True(true);
+    }
+}
